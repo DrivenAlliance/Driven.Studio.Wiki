46b053f9fd40b66483c30ea21fbfc2e0da2cb520
Janco <jancow@gmail.com>
2017-02-06 07:46:19 +0200

    Support relative paths for wiki content - rooted to the application path by default

--- MarkdownWiki/Settings.cs
+++ MarkdownWiki/Settings.cs
@@ -1,7 +1,24 @@
+using System.IO;
+using System.Web;
+
 namespace MarkdownWiki
 {
     public class Settings
     {
-        public static string WikiPath => Properties.Settings.Default.WikiFilePath;
+        public static string WikiPath
+        {
+            get
+            {
+                var wikiContentPath = Properties.Settings.Default.WikiFilePath;
+                return Path.IsPathRooted(wikiContentPath)
+                    ? wikiContentPath
+                    : rootInAppPath(wikiContentPath);
+            }
+        }
+
+        private static string rootInAppPath(string configuredPath)
+        {
+            return Path.Combine(HttpRuntime.AppDomainAppPath, configuredPath);
+        }
     }
 }

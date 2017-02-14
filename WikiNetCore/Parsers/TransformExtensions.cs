bebd63689967c75f6558eee6658023844fdf7949
Janco <jancow@gmail.com>
2017-02-14 08:30:55 +0200

    more refactoring

--- WikiNetCore/Parsers/TransformExtensions.cs
+++ WikiNetCore/Parsers/TransformExtensions.cs
@@ -53,20 +53,56 @@ namespace MarkdownWiki.Parsers
 
         private static void fixupLinks(string contentPath, MarkdownDocument markdownDocument)
         {
-            var links = markdownDocument.Descendants().OfType<LinkInline>();
-            foreach (var link in links)
-            {
-                Uri linkUri;
-                if (Uri.TryCreate(link.Url, UriKind.RelativeOrAbsolute, out linkUri))
-                {
-                    if (!linkUri.IsAbsoluteUri) // only rewrite relative links to other local content
-                    {
-                        link.Url = link.IsImage
-                            ? linkToStaticContent(contentPath, linkUri)
-                            : linkToDynamicContent(contentPath, linkUri);
-                    }
-                }
-            }
+            var localLinks = markdownDocument.Descendants().OfType<LinkInline>()
+                .Select(l => new MarkdownLinkUriPair(l))
+                .Where(l => l.IsValidLocalLink());
+
+            foreach (var link in localLinks)
+                link.Fixit(contentPath);
+        }
+
+        private static string fixUpTableOfContents(string content)
+        {
+            var tableOfContentsParser = new TableOfContentsParser();
+            return tableOfContentsParser.Parse(content);
+        }
+
+        private static string absoluteLocalFilePath(string pathRelativeToWikiContent)
+        {
+            var location = Settings.WikiContentPathUri();
+            return Path.Combine(location.LocalPath, pathRelativeToWikiContent);
+        }
+    }
+
+    internal class MarkdownLinkUriPair
+    {
+        private readonly LinkInline _markdownLink;
+        private readonly Uri _uri;
+
+        public MarkdownLinkUriPair(LinkInline markdownLink)
+        {
+            _markdownLink = markdownLink;
+            Uri linkUri;
+            if (Uri.TryCreate(markdownLink.Url, UriKind.RelativeOrAbsolute, out linkUri))
+                _uri = linkUri;
+        }
+
+        public bool IsValidLocalLink()
+        {
+            return _uri != null && !_uri.IsAbsoluteUri;
+        }
+
+        public void Fixit(string contentPath)
+        {
+            _markdownLink.Url = linksToStaticContent()
+                ? linkToStaticContent(contentPath, _uri)
+                : linkToDynamicContent(contentPath, _uri);
+        }
+
+        private bool linksToStaticContent()
+        {
+            // todo: consider better way of checking for content to be generated instead of this flag
+            return _markdownLink.IsImage;
         }
 
         private static string linkToDynamicContent(string contentPath, Uri linkUri)
@@ -83,18 +119,6 @@ namespace MarkdownWiki.Parsers
             var wikiContentRoot = "/wikicontent";
             var imgPath = linkUri.ToString();
             return $"{wikiContentRoot}/{contentPath}/{imgPath}";
-        }
-
-        private static string fixUpTableOfContents(string content)
-        {
-            var tableOfContentsParser = new TableOfContentsParser();
-            return tableOfContentsParser.Parse(content);
-        }
-
-        private static string absoluteLocalFilePath(string pathRelativeToWikiContent)
-        {
-            var location = Settings.WikiContentPathUri();
-            return Path.Combine(location.LocalPath, pathRelativeToWikiContent);
         }
     }
 }

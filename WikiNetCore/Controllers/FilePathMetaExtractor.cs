5742d1559923fa601f03d8dd7938c15cc8060759
Janco <jancow@gmail.com>
2017-02-15 06:51:43 +0200

    Remove dead code

--- WikiNetCore/Controllers/FilePathMetaExtractor.cs
+++ WikiNetCore/Controllers/FilePathMetaExtractor.cs
@@ -1,31 +1,10 @@
 using System;
-using System.IO;
 
 namespace MarkdownWiki.Controllers
 {
     public static class FilePathMetaExtractor
     {
-        public static string GetRelativePath(this string entry)
-        {
-            return entry.Contains(@"\") ? entry.Substring(0, entry.LastIndexOf(@"\", StringComparison.InvariantCultureIgnoreCase) + 1) : String.Empty;
-        }
-
-        public static string EscapeFilePathEnd(this string filePath)
-        {
-            if (filePath.LastIndexOf(@"\", StringComparison.InvariantCultureIgnoreCase) + 1 == filePath.Length)
-            {
-                filePath += @"\";
-            }
-            return filePath;
-        }
-
         public static string NormalizeFileName(this string fileName)
-        {
-            //return fileName.Replace(Settings.WikiPath, String.Empty).Replace(".md", String.Empty);
-            return NFN(fileName);
-        }
-
-        public static string NFN(this string fileName)
         {
             var uri = new Uri(fileName);
             var result = Settings.WikiContentPathUri().MakeRelativeUri(uri);

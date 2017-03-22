4a684bfbb1ba243984d500361e739f2680ba0fdb
Janco <jancow@gmail.com>
2017-03-22 08:44:43 +0200

    Add projects / libs / config to enable running .net core tests & reorg solution structure to follow commonly followed src + test convention

---
+++ WikiNetCoreTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WikiNetCoreTests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a386d5db-59eb-4a32-877e-1e8eeee85463")]

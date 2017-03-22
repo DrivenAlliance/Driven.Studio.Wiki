95369389a28e0b1e7d04cc088aafaf4d734da200
Janco <jancow@gmail.com>
2017-03-22 06:36:29 +0200

    Fix issue where files in the root wiki content dir were not being displayed in the tree menu

--- WikiNetCore/Controllers/MenuTreeCreator.cs
+++ WikiNetCore/Controllers/MenuTreeCreator.cs
@@ -16,26 +16,41 @@ namespace WikiNetCore.Controllers
 
         public IEnumerable<MenuTreeNode> GetMenuTreeForDirectory(string parentDir)
         {
-            return Directory.GetDirectories(parentDir)
-                .OrderBy(dir => dir)
-                .Select(dir =>
-                    new MenuTreeNode()
-                    {
-                        text = dir.Replace(parentDir, ""),
-                        nodes = GetMenuTreeForDirectory(dir).Union(getFiles(dir))
-                    })
+            return
+                buildSubDirectoryNodes(parentDir)
+                .Union(buildFileNodes(parentDir))
                 .ToList();
         }
 
-        private IEnumerable<MenuTreeNode> getFiles(string dir)
+        private IEnumerable<MenuTreeNode> buildSubDirectoryNodes(string parentDir)
         {
-            return Directory.GetFiles(dir,"*.md")
-                .Select(fileName =>
-                    new MenuTreeNode()
-                    {
-                        text = fileName.Replace(dir,"").Replace(".md","").Replace("\\",""),
-                        href = createLinkFromFileName(fileName)
-                    });
+            return Directory.GetDirectories(parentDir)
+                .OrderBy(dir => dir)
+                .Select(dir => buildDirectoryNode(parentDir, dir));
+        }
+
+        private MenuTreeNode buildDirectoryNode(string directory, string dir)
+        {
+            return new MenuTreeNode()
+            {
+                text = dir.Replace(directory, ""),
+                nodes = GetMenuTreeForDirectory(dir)
+            };
+        }
+
+        private IEnumerable<MenuTreeNode> buildFileNodes(string dir)
+        {
+            return Directory.GetFiles(dir, "*.md")
+                .Select(fileName => buildFileNode(dir, fileName));
+        }
+
+        private MenuTreeNode buildFileNode(string dir, string fileName)
+        {
+            return new MenuTreeNode()
+            {
+                text = fileName.Replace(dir, "").Replace(".md", "").Replace("\\", ""),
+                href = createLinkFromFileName(fileName)
+            };
         }
 
         private string createLinkFromFileName(string fileName)
